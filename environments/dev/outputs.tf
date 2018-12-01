6059be4d20ae12f7c61cc950557813f2130317cf
Grégory Fouquet <gfouquet@armida.fr>
2018-12-01 16:18:56 +0100

    Reworked terraform output as a map

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,6 +1,14 @@
-output "kube_masters_public_ips" {
-  value = ["${google_compute_instance.kube-master.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+output "kube_masters" {
+  value = {
+    names = ["${google_compute_instance.kube-master.*.name}"]
+    public_ips = ["${google_compute_instance.kube-master.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+    private_ips = ["${google_compute_instance.kube-master.*.network_interface.0.network_ip}"]
+  }
 }
-output "kube_workers_public_ips" {
-  value = ["${google_compute_instance.kube-worker.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+output "kube_workers" {
+  value = {
+    names = ["${google_compute_instance.kube-worker.*.name}"]
+    public_ips = ["${google_compute_instance.kube-worker.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+    private_ips = ["${google_compute_instance.kube-worker.*.network_interface.0.network_ip}"]
+  }
 }
